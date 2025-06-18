92b2c36f50d959b3cc7172d7c6813ad3c23c164c
Jim Mattson <jsmattsonjr@gmail.com>
2025-06-18 16:10:25 -0700

    Don't export any APIs
    This is just a command-line tool.
    
    Signed-off-by: Jim Mattson <jsmattsonjr@gmail.com>

--- src/brunnels/__init__.py
+++ src/brunnels/__init__.py
@@ -10,17 +10,3 @@ import importlib.metadata
 __version__ = importlib.metadata.version("brunnels")
 __author__ = "Jim Mattson"
 __email__ = "jsmattsonjr@gmail.com"
-
-# Import main classes for public API
-from .brunnel import Brunnel, BrunnelType, FilterReason, RouteSpan
-from .route import Route
-from .geometry import Position
-
-__all__ = [
-    "Brunnel",
-    "BrunnelType",
-    "FilterReason",
-    "RouteSpan",
-    "Route",
-    "Position",
-]
